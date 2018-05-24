f69d79b94cb0ae4118defaa49ad195992e2a413d
pengtoxen <82912094@qq.com>
2018-05-24 19:21:46 +0800

    up

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -55,7 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
-      title: 'vue-element-admin',
+      title: 'decode-nature-cms',
       path: config.dev.assetsPublicPath + config.dev.assetsSubDirectory
     }),
   ]
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -68,7 +68,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       favicon: resolve('favicon.ico'),
-      title: 'vue-element-admin',
+      title: 'decode-nature-cms',
       path: config.build.assetsPublicPath + config.build.assetsSubDirectory,
       minify: {
         removeComments: true,
